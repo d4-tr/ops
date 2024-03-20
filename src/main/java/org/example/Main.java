f3d3c9499e82d6493c2c405e63f5d1809de1c97d
hzrncik <hzrncik@redhat.com>
2024-03-20 22:27:01 +0100

    checkstyle fix in code
    Signed-off-by: hzrncik <hzrncik@redhat.com>

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,9 @@
 package org.example;
 
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
-
 final class Main {
 
-    private static final Logger logger = LogManager.getLogger(Main.class);
-    public static void main(String[] args) {
-        CustomLogging.setup();
+    // private static final Logger logger = LogManager.getLogger(Main.class);
+    public static void main(final String[] args) {
 
-    }}
+    }
+}
