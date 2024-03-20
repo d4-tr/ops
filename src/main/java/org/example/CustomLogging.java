9fb714c712169c7b75f1339e9a448cb76c9e1351
hzrncik <hzrncik@redhat.com>
2024-03-20 22:21:18 +0100

    setup logging
    Signed-off-by: hzrncik <hzrncik@redhat.com>

--- src/main/java/org/example/CustomLogging.java
+++ src/main/java/org/example/CustomLogging.java
@@ -1,13 +1,12 @@
 package org.example;
 
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-final class Main {
+public class CustomLogging {
 
     private static final Logger logger = LogManager.getLogger(Main.class);
-    public static void main(String[] args) {
-        CustomLogging.setup();
+    public static void setup(){
 
-    }}
+    }
+}
